feb14606f0d0f65d46862733157aab1387599e5f
Ahmad Essam <61994278+AHMAD19858@users.noreply.github.com>
2022-11-03 22:00:38 +0200

    animate city's list

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,46 +1,61 @@
-import React, { useContext, useState } from 'react';
-import { Text, View, FlatList, TouchableOpacity, KeyboardAvoidingView, Switch } from 'react-native'
+import React, { useContext, useRef } from 'react';
+import { Text, View, FlatList, TouchableOpacity, KeyboardAvoidingView, StatusBar, Animated } from 'react-native'
 import styles from './styles'
 import Icon from 'react-native-vector-icons/AntDesign';
 import IconCity from 'react-native-vector-icons/FontAwesome5';
-import {  WeatherContext } from '../../context/WeatherContext'
+import { WeatherContext } from '../../context/WeatherContext'
 import IconPlus from 'react-native-vector-icons/AntDesign';
 import { COLORS } from '../../constants';
 import { EventRegister } from 'react-native-event-listeners';
-const Home = ({ navigation }) => {
-    const { state,ThemeContext } = useContext(WeatherContext)
- 
+const SPACING = 10; //20
+const AVATAR_SIZE = 170;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3
 
-    const [mode, setMode] = useState(false)
-    console.log("dddd",ThemeContext)
+const Home = ({ navigation }) => {
+    const { state } = useContext(WeatherContext)
+    const scrollY = useRef(new Animated.Value(0)).current;
+
     return (
-        <View style={[styles.container, { backgroundColor: ThemeContext.background }]}>
+        <View style={styles.container}>
             <View style={styles.appbar}>
             </View>
             <Text style={styles.homeTitle}>Cities</Text>
             {
                 state.length == '' ?
                     <View>
-                        <Switch value={mode} onValueChange={(value) => {
-                            setMode(value);
-                            EventRegister.emit("changeTheme", value)
 
-                        }
-
-                        } />
                         <Text style={styles.empty}>List is empty, Please add a city first!</Text>
                     </View>
                     :
                     <View>
-                        <FlatList contentContainerStyle={{ alignItems: 'center' }}
-                            onEndReachedThreshold={0.2}
-                            progressViewOffset={1}
-                            scrollEventThrottle={1}
-                            keyExtractor={(item, index) => String(index)}
+                        <Animated.FlatList
+                            onScroll={Animated.event(
+                                [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                                { useNativeDriver: true }
+                            )}
+                            contentContainerStyle={{
+                                alignItems: 'center',
+                                flexGrow: 1,
+                                paddingBottom: 250,
+                                paddingTop: StatusBar.currentHeight || 42
+                            }}
+                            keyExtractor={(item) => item.id}
+                            scrollEnabled={true}
                             data={state}
                             renderItem={({ item, index }) => {
+                                const inputRange = [
+                                    -1,
+                                    0,
+                                    ITEM_SIZE * index,
+                                    ITEM_SIZE * (index + 2),
+                                ]
+
+                                const scale = scrollY.interpolate({
+                                    inputRange,
+                                    outputRange: [1, 1, 0.8, 0]
+                                })
                                 return (
-                                    <View style={{ ...styles.card }}>
+                                    <Animated.View style={{ transform: [{ scale }], ...styles.card }}>
 
                                         <View style={styles.row} >
                                             <View style={styles.row}>
@@ -68,7 +83,7 @@ const Home = ({ navigation }) => {
                                                 />
                                             </TouchableOpacity>
                                         </View>
-                                    </View>
+                                    </Animated.View>
                                 )
                             }}
                         />
